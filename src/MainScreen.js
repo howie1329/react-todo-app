08c4aa1d774c7825ff17bc00f6bb646f7bfc48e9
howie1329 <46204910+howie1329@users.noreply.github.com>
2022-10-01 22:39:12 -0400

    second update

--- src/MainScreen.js
+++ src/MainScreen.js
@@ -8,7 +8,7 @@ function MainScreen ({todos,remove,edit,body}){
     return(
         <div id = 'main-screen'>
             <h1 id = 'main-header'>TODO APP</h1>
-            <Link id = 'form-title' to = './form'>Form</Link>
+            <Link id = 'form-title' to = './form'>New Todo?</Link>
             <div id = 'todo-container'>
                 {todos.map((titles,index) => <Todo key = {index} remove = {remove} edit = {edit} index = {index} title = {titles} body = {body[index]}/>)}
             </div>
